31616b32f31a67ac88f69305fc23cecdab2c3956
Ibnu Daqiqil Id <ibnu.daqiqil@lecturer.unri.ac.id>
2020-03-26 15:40:17 +0900

    add dataset gaji

--- Bab 2. Teori Pendukung/plot01.py
+++ Bab 2. Teori Pendukung/plot01.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 try:
-    salary = pd.read_csv("../data/salary_table.csv")
+    salary = pd.read_csv("../DATASET/gaji.csv")
 except:
-    url = 'https://raw.github.com/duchesnay/pylearn-doc/master/data/salary_table.csv'
+    url = '/gaji.csv'
     salary = pd.read_csv(url)
 df = salary
+label = {'S1':'r', 'S2':'g', 'S3':'blue'}
+fig, ax = plt.subplots()
+plt.xlabel('Pengalaman (tahun)')
+plt.ylabel('Gaji ($)')
+plt.title('Grafik Gaji berdasarkan Pengalaman dan Pendidikan')
+#plt.grid(True)
+
+for key in label:
+    X=df[df['Pendidikan']==key]['Pengalaman']
+    Y=df[df['Pendidikan']==key]['Gaji']
+    ax.scatter(X, Y, c=label[key],label=key, s=100)
+
+ax.legend()
+plt.show()
